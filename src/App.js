16ed0168e821d528dfb017e9a0ee08a272860d70
idindrakusuma <indrakusuma.udinus@gmail.com>
2018-08-17 10:11:18 +0700

    functional component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      return (
+        <div className="App title">
+            <h1> Awesome React!</h1>
+            <p> This is really awesome!</p>
+            {/* use the component in react */}
+            <Person />
+        </div>
+        // React.createElement('div', {className: 'App'}, 'This is awesome!')
+      );
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const person = () => {
+    return (
+        <div>
+            <p>Hello! I'm person!</p>
+            <p>Hello! I'm jenius!</p>
+        </div>
+    );
+}
+
+export default person
