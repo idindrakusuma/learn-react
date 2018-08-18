4eab61a9716ffbcd6080cd1c22db349f013efd74
idindrakusuma <indrakusuma.udinus@gmail.com>
2018-08-18 15:10:04 +0700

    learn about if statement and list statement in jsx

--- src/App.js
+++ src/App.js
@@ -1,1 +1,94 @@
-import React, {Component} from 'react';import './App.css';import Person from './Person/Person'class App extends Component {    state = {        persons: [            {name: 'Indra', age: 20},            {name: 'Dinda', age: 21}        ]    }    changeStateHandler = (newName) => {        this.setState({            persons: [                {name: newName, age: 22},                {name: 'Supriyadi', age: 25}            ]        })    }    changeNameHandler = (event) => {        this.setState({            persons: [                {name: 'Indra', age: 22},                {name: event.target.value, age: 25}            ]        })    }  render() {      const styleButtonInline = {          backgroundColor: 'red',          borderColor: 'red',          textTransform: 'uppercase'      }      return (        <div className="container">            <h1> Awesome React!</h1>            <p> This is really awesome!</p>            {/* use the component in react */}            <button onClick={() => this.changeStateHandler('Santriwati')} style={styleButtonInline}                    className="btn btn-info"> Change State </button>            <div className="row mt-2">                <div className="col-md-6">                    <Person name={this.state.persons[0].name}                            age={this.state.persons[0].age}                            click={this.changeStateHandler.bind(this, 'Santri!')}/>                </div>                <div className="col-md-6">                    <Person name={this.state.persons[1].name}                            age={this.state.persons[1].age}                            changed={this.changeNameHandler}/>                </div>            </div>        </div>      );  }}export default App;
+import React, {Component} from 'react';
+import './App.css';
+import Person from './Person/Person'
+
+class App extends Component {
+	state = {
+		persons: [
+				{id: 1, name: 'Indra', age: 20},
+				{id: 2, name: 'Dinda', age: 21},
+				{id: 3, name: 'Saraswati', age: 24}
+		],
+		showPerson: false
+	}
+
+	changeStateHandler = (newName) => {
+		this.setState({
+				persons: [
+						{id: 2, name: newName, age: 21},
+						{id: 3, name: 'Saraswati', age: 24}
+				]
+		})
+	}
+
+	changeNameHandler = (event, id) => {
+		/* first, find index of object */
+		const personIndex = this.state.persons.findIndex(p => {
+			return p.id === id;
+		})
+		/* get value object from original object state */
+		const person = {
+			...this.state.persons[personIndex]
+		}
+		/* change the name from value input */
+		person.name = event.target.value;
+		/* make persons with the value from older value state person */
+		const persons = [...this.state.persons];
+		/* updated with new value */
+		persons[personIndex] = person;
+		/* updated in state */
+		this.setState({
+				persons: persons
+		})
+	}
+
+	deletePersonHandler = (personIndex) => {
+		const persons = [...this.state.persons].splice(personIndex, 1);
+		this.setState({
+				persons: persons
+		})
+	}
+
+	showHidePerson = () => {
+		const doesPerson = this.state.showPerson;
+		this.setState({ showPerson : !doesPerson})
+	}
+
+  render() {
+      const styleButtonInline = {
+          textTransform: 'uppercase'
+      }
+
+      /* if using logic in jsx, better than login in javascript */
+      let persons = null;
+
+      if (this.state.showPerson) {
+          persons = (
+                  this.state.persons.map((person, index) => {
+                      return <Person name={person.name}
+                                     key={person.id}
+                                     age={person.age} delete={() => this.deletePersonHandler(index)}
+                                     changed={(event) => this.changeNameHandler(event, person.id)}/>
+                    })
+          );
+      }
+
+      return (
+        <div className="container">
+					<div className="card mt-4">
+						<h5 className="card-header">Awesome React!</h5>
+						<div className="card-body">
+							<div>
+									<button onClick={() => this.changeStateHandler('Santriwati')} style={styleButtonInline}
+													className="btn btn-danger"> Change State </button>
+									<button onClick={this.showHidePerson} className="btn btn-info ml-2"> Show/Hide </button>
+							</div>
+						</div>
+					</div>
+            { persons }
+        </div>
+      );
+  }
+}
+
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,10 +3,20 @@ import "./Person.css"
 
 const person = (props) => {
     return (
-        <div className="card">
-            <div className="card-body">
-                <p onClick={props.click}>Hello! I'm {props.name}! {props.age} years old! {props.children}</p>
-                <input type="text" onChange={props.changed} value={props.name}/>
+        <div className="mt-1 mb-1">
+            <div className="card">
+								<div className="card-header">
+                    <p onClick={props.click}>Hello! I'm {props.name}! {props.age} years old! {props.children}</p>
+								</div>
+                <div className="card-body">
+										<input type="text" placeholder="Input your own name.."
+											className="form-control" 
+											onChange={props.changed}
+											value={props.name} />
+                </div>
+								<div className="card-footer">
+                    <button className="btn btn-danger btn-sm float-right" onClick={props.delete}>Delete</button>
+								</div>
             </div>
         </div>
     );
